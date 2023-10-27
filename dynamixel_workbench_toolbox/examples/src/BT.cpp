539157e9bae9862070750bf666f432fcd2ec396a
simion <simion@simisoft.co.uk>
2023-10-27 22:00:29 +0000

    prep managed objects for service & characteristic

--- dynamixel_workbench_toolbox/examples/src/BT.cpp
+++ dynamixel_workbench_toolbox/examples/src/BT.cpp
@@ -14,13 +14,22 @@ int main(int argc, char *argv[])
 
     // Create a proxy object for the BlueZ root object
     const char* serviceName = "org.bluez";
-    const char* objectPath = "/org/bluez/hci0";
-    auto btProxy = sdbus::createProxy(*connection, serviceName, objectPath);
+    const char* adapterPath = "/org/bluez/hci0";
+    auto adapterProxy = sdbus::createProxy(*connection, serviceName, adapterPath);
 
+    const char* managerPath = "/";
+    auto managerProxy = sdbus::createProxy(*connection, serviceName, managerPath);
+
+    std::string introspectionXML;
+    adapterProxy->callMethod("Introspect")
+                   .onInterface("org.freedesktop.DBus.Introspectable")
+                   .storeResultsTo(introspectionXML);
+
+    // std::cout << introspectionXML << std::endl;
     // Get the "Address" property
     std::string address;
     try {
-        auto addressVariant = btProxy->getProperty("Address").onInterface("org.bluez.Adapter1");
+        auto addressVariant = adapterProxy->getProperty("Address").onInterface("org.bluez.Adapter1");
         address = addressVariant.get<std::string>();
         std::cout << "Address: " << address << std::endl;
     } catch (const sdbus::Error& e) {
@@ -30,13 +39,33 @@ int main(int argc, char *argv[])
     // Get the "Name" property
     std::string name;
     try {
-        auto nameVariant = btProxy->getProperty("Name").onInterface("org.bluez.Adapter1");
+        auto nameVariant = adapterProxy->getProperty("Name").onInterface("org.bluez.Adapter1");
         name = nameVariant.get<std::string>();
         std::cout << "Name: " << name << std::endl;
     } catch (const sdbus::Error& e) {
         std::cerr << "Failed to get Name property: " << e.what() << std::endl;
     }
+    try {
 
+        std::map<sdbus::ObjectPath, std::map<std::string, std::map<std::string, sdbus::Variant>>> managedObjects;
+        managerProxy->callMethod("GetManagedObjects").onInterface("org.freedesktop.DBus.ObjectManager").storeResultsTo(managedObjects);
+
+        for (const auto& [objectPath, interfaces] : managedObjects) {
+            std::cout << "Object: " << objectPath << std::endl;
+            for (const auto& [interfaceName, properties] : interfaces) {
+                std::cout << "  Interface: " << interfaceName << std::endl;
+                for (const auto& [propertyName, value] : properties) {
+                    std::cout << "    Property: " << propertyName << std::endl;
+                }
+            }
+        }
+    }
+    catch (const sdbus::Error& error) {
+        std::cerr << "D-Bus error: " << error.getName() << " - " << error.getMessage() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "An error occurred: " << e.what() << std::endl;
+    }
 
     return 0;
 }
